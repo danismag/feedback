fcec825efb41a67f929c7f8d37a394468036f5d2
danis <danis29@bk.ru>
2016-11-22 14:37:00 +0300

    добавил 404

---
+++ App/templates/404.php
@@ -0,0 +1,42 @@
+<?php if(false): ?>
+<!--
+    Шаблон 404 страницы
+    
+-->
+<?php endif; ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+     <link href="/App/css/feedmain.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Главная</a>
+            </div>
+        </div>
+    </nav>
+       
+    <!-- контейнер для отзывов -->
+    <div class="container" id="main">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+            
+            <h1><p class="text-center">Вы ввели несуществующий адрес</p></h1>
+            <h3><p class="text-center"><a href="/">на главную</a></p></h3>
+            
+            </div>
+        </div>
+    </div>
+
+    <script src="/js/jquery-3.1.0.js"></script>
+    <script src="/js/jquery.form.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/App/js/feedjs.js"></script>
+  </body>
+</html>
